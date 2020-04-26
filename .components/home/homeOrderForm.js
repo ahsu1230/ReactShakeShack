ae0957b585d92a24dcd339634b9d4f7c5d11ed9c
Aaron Hsu <aaron@nextbit.com>
2020-04-26 14:13:29 -0400

    Runthrough input onchange

--- .components/home/homeOrderForm.js
+++ .components/home/homeOrderForm.js
@@ -7,10 +7,39 @@ export class HomeOrderForm extends React.Component {
         console.log("Add Order!");
     }
 
+    onChangeSelect = (event) => {
+        console.log("Select: " + event.target.value);    
+    }
+
+    onChangeNum = (event) => {
+        console.log("Num: " + event.target.value);    
+    }
+
+    onChangeName = (event) => {
+        console.log("Name: " + event.target.value);        
+    }
+
     render() {
         return (
             <section id="home-order-form">
                 <h2>New Order Form</h2>
+
+                <select onChange={(e) => this.onChangeSelect(e)}>
+                    <option>Burgers</option>
+                    <option>Fries</option>
+                    <option>Shakes</option>
+                </select>
+
+                <span>Number:</span>
+                <input type="text" placeholder="1" 
+                    className="num"
+                    onChange={(event) => this.onChangeNum(event)}/>
+
+                <span>Your Name:</span>
+                <input type="text" placeholder="Your name" 
+                    className="name"
+                    onChange={(event) => this.onChangeName(event)}/>
+
                 <button onClick={this.onClickAddButton}>Add To Orders</button>
             </section>
         );
