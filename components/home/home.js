a2b6eabe3d6f9d263d7c5c3d26dbe5128c7e8ff8
Tony Wu <tonywu315@gmail.com>
2020-06-07 17:22:41 -0400

    Added header

--- components/home/home.js
+++ components/home/home.js
@@ -5,6 +5,7 @@ import React from "react";
 export default class HomePage extends React.Component {
     render() {
         return (
+            <h1>Shake Shack Order Manager</h1>
             <div id="view-home">
                 <h1>Home</h1> {/* Replace this header */}
             </div>
