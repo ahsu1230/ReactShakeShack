f6840f7f88290c41166c433d7d26a4c2cfb3057f
Tony Wu <tonywu315@gmail.com>
2020-06-07 17:27:30 -0400

    Fixed header

--- components/home/home.js
+++ components/home/home.js
@@ -5,9 +5,8 @@ import React from "react";
 export default class HomePage extends React.Component {
     render() {
         return (
-            <h1>Shake Shack Order Manager</h1>
             <div id="view-home">
-                <h1>Home</h1> {/* Replace this header */}
+                <h1>Shake Shack Order Manager</h1>
             </div>
         );
     }
